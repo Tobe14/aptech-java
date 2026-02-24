c6f92bee6020eea0cf5cbe0d80d348e423d1ea23
Tobe14 <thuggt450@gmail.com>
2026-02-24 11:06:06 +0100

    first commit

---
+++ src/So.java
@@ -0,0 +1,10 @@
+public class So {
+
+
+    static void main() {
+        int itemPrice = 56;
+        int shippingCost = 45;
+        int sum = itemPrice + shippingCost;
+        System.out.println(sum);
+    }
+}
