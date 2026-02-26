1bcf215f7240402197a33741cda7e4484d1a2429
Tobe14 <thuggt450@gmail.com>
2026-02-26 11:43:52 +0100

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,10 @@
+public class Data {
+    static void main() {
+        int studentID = 11;
+        double score = 90.00;
+        char grade = 'A';
+        System.out.println(studentID);
+        System.out.println(score);
+        System.out.println(grade);
+    }
+}
