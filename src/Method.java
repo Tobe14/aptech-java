7e76628b1632f12beaab1e8a65e81ecc8c97cac9
Tobe14 <thuggt450@gmail.com>
2026-03-08 23:51:58 +0100

    first  commit

---
+++ src/Method.java
@@ -0,0 +1,10 @@
+public class Method {
+    static void myMethod() {
+        System.out.println("Thats how we run things");
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
---
+++ src/MethodWork.java
@@ -0,0 +1,9 @@
+public class MethodWork {
+    static void welcomeMessage() {
+        System.out.println("Welcome to Java");
+    }
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
+
+    }
